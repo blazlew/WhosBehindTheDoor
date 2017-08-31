a05d11043b88916a33f355a36476ab61b367fbca
Michał Bodzioch <Michal.Bodzioch@ttpsc.pl>
2017-08-31 10:02:32 +0200

    Activities added, basic button action added

--- app/src/main/java/com/incognitodevs/wibtd/MainActivity.kt
+++ app/src/main/java/com/incognitodevs/wibtd/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.incognitodevs.wibtd
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        cameraButton.setOnClickListener {
+            val intent = Intent(this, CameraActivity::class.java)
+            startActivity(intent)
+        }
+
+        settingsButton.setOnClickListener {
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
+        }
+
+        galleryButton.setOnClickListener {
+            val intent = Intent(this, GalleryActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
