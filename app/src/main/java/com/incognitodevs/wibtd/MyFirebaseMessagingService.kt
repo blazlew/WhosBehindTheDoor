bf60587ccb278555a8ffff11168fae3fc90436ab
Michał Bodzioch <Michal.Bodzioch@ttpsc.pl>
2017-08-31 15:09:26 +0200

    receiving notifications added

---
+++ app/src/main/java/com/incognitodevs/wibtd/MyFirebaseMessagingService.kt
@@ -0,0 +1,20 @@
+package com.incognitodevs.wibtd
+
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+    private val TAG = "FCM Service"
+    override fun onMessageReceived(remoteMessage: RemoteMessage?) {
+        // TODO: Handle FCM messages here.
+        // If the application is in the foreground handle both data and notification messages here.
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated.
+        Log.d(TAG, "From: " + remoteMessage!!.from)
+        Log.d(TAG, "Notification Message Body: " + remoteMessage.notification.body!!)
+    }
+
+}
