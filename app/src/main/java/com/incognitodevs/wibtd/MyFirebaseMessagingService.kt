f6ad410cfad0fd0b5eceebf009f48d4d9b8ad29b
Michał Bodzioch <Michal.Bodzioch@ttpsc.pl>
2017-09-04 11:37:20 +0200

    graphic representation of notification added

--- app/src/main/java/com/incognitodevs/wibtd/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/incognitodevs/wibtd/MyFirebaseMessagingService.kt
@@ -3,11 +3,20 @@ package com.incognitodevs.wibtd
 import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
-
+import android.content.Context.NOTIFICATION_SERVICE
+import android.app.NotificationManager
+import android.media.RingtoneManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.support.v4.app.NotificationCompat
 
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
     private val TAG = "FCM Service"
+    var notificationTitle: String? = null
+    var notificationBody:String? = null
+
     override fun onMessageReceived(remoteMessage: RemoteMessage?) {
         // TODO: Handle FCM messages here.
         // If the application is in the foreground handle both data and notification messages here.
@@ -15,6 +24,28 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         // message, here is where that should be initiated.
         Log.d(TAG, "From: " + remoteMessage!!.from)
         Log.d(TAG, "Notification Message Body: " + remoteMessage.notification.body!!)
+        notificationTitle = remoteMessage.notification.title
+        notificationBody = remoteMessage.notification.body
+
+        sendNotification(notificationTitle!!, notificationBody!!)
     }
 
+    private fun sendNotification(notificationTitle: String, notificationBody: String) {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent,
+                PendingIntent.FLAG_ONE_SHOT)
+
+        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder = NotificationCompat.Builder(this)
+                .setAutoCancel(true)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentIntent(pendingIntent)
+                .setContentTitle(notificationTitle)
+                .setContentText(notificationBody)
+                .setSound(defaultSoundUri) as NotificationCompat.Builder
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(0, notificationBuilder.build())
+    }
 }
