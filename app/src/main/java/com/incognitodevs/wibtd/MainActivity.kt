f9e5cc523b29f602b869bf6a0ee0acd20519b07e
ledsoon <ledek89@gmail.com>
2017-10-10 03:04:56 +0200

    Menu changed to 'Space Navigation View' - (https://android-arsenal.com/details/1/4180)

--- app/src/main/java/com/incognitodevs/wibtd/MainActivity.kt
+++ app/src/main/java/com/incognitodevs/wibtd/MainActivity.kt
@@ -3,6 +3,9 @@ package com.incognitodevs.wibtd
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.luseen.spacenavigation.SpaceItem
+import com.luseen.spacenavigation.SpaceOnClickListener
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,20 +14,35 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        cameraButton.setOnClickListener {
-            val intent = Intent(this, CameraActivity::class.java)
-            startActivity(intent)
-        }
+        spaceNavigationView.initWithSaveInstanceState(savedInstanceState)
+        spaceNavigationView.addSpaceItem(SpaceItem("GALLERY", R.drawable.ic_gallery))
+        spaceNavigationView.addSpaceItem(SpaceItem("SETTINGS", R.drawable.ic_settings))
+        spaceNavigationView.setCentreButtonIcon(R.drawable.ic_camera)
+        spaceNavigationView.setSpaceOnClickListener(object: SpaceOnClickListener {
+            override fun onCentreButtonClick() {
+                val intent = Intent(this@MainActivity, CameraActivity::class.java)
+                startActivity(intent)
+            }
 
-        settingsButton.setOnClickListener {
-            val intent = Intent(this, SettingsActivity::class.java)
-            startActivity(intent)
-        }
+            override fun onItemClick(itemIndex: Int, itemName: String) {
+                if(itemIndex == 0 ){
+                    val intent = Intent(this@MainActivity, GalleryActivity::class.java)
+                    startActivity(intent)
+                }else if(itemIndex == 1){
+                    val intent = Intent(this@MainActivity, SettingsActivity::class.java)
+                    startActivity(intent)
+                }
+            }
 
-        galleryButton.setOnClickListener {
-            val intent = Intent(this, GalleryActivity::class.java)
-            startActivity(intent)
-        }
+            override fun onItemReselected(itemIndex: Int, itemName: String) {
+                Toast.makeText(this@MainActivity, itemIndex.toString() + " " + itemName, Toast.LENGTH_SHORT).show()
+            }
 
+        })
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        spaceNavigationView.onSaveInstanceState(outState)
     }
 }
